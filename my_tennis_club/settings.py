3ef51aaa371d144177bba745bacb8f5388df9dc5
smile881225 <53891140+smile881225@users.noreply.github.com>
2024-05-30 07:35:12 +0800

    解除帳號密碼限制

--- my_tennis_club/settings.py
+++ my_tennis_club/settings.py
@@ -91,12 +91,14 @@ DATABASES = {
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
+    # {
+    #  帳號密碼太相似的限制
+    #     'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    # },
+    # {
+    #     密碼最短限制
+    #     'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    # },
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
     },
